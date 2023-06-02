84848db2b25de07244360782be340428d67d6995
wdev733 <wdev733@gmail.com>
2023-06-03 00:01:07 +0800

    change to infura

--- src/index.js
+++ src/index.js
@@ -12,14 +12,14 @@ const { cERC20, erc20, comptroller, priceOracle2 } = require("./abis");
 const { bigDecimal, comptrollers } = require("./utils/constant");
 
 const port = process.env.PORT || "8001";
-// const infura_api_key = process.env.INFURA_API_KEY;
+const infura_api_key = process.env.INFURA_API_KEY || "1181bcdcdbbb4acbb4eaa07e11c33e66";
 
-// // Using Infura WebSockets
-// const provider = new Web3.providers.WebsocketProvider(
-//   `wss://mainnet.infura.io/ws/v3/${infura_api_key}`
-// )
-// const web3 = new Web3(provider);
-const web3 = new Web3("https://eth-mainnet.gateway.pokt.network/v1/lb/1ff81eec8bb84b6f98ae97a2");
+// Using Infura WebSockets
+const provider = new Web3.providers.WebsocketProvider(
+  `wss://mainnet.infura.io/ws/v3/${infura_api_key}`
+)
+const web3 = new Web3(provider);
+// const web3 = new Web3("https://eth-mainnet.gateway.pokt.network/v1/lb/1ff81eec8bb84b6f98ae97a2");
 
 const getMarkets = async (comp, underlyingPriceDecimal = 18) => {
   let result = {};
